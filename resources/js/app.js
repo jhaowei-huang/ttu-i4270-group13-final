b156dd7381981152bfdcbef826d0053174cd97b0
jhaowei-huang <jhaowei.huang@gmail.com>
2018-12-22 02:06:19 +0800

    install and run node_modules

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First, we will load all of this project's Javascript utilities and other
  * dependencies. Then, we will be ready to develop a robust and powerful
@@ -7,8 +6,5 @@
 
 require('./bootstrap');
 
-/**
- * 增加Bootstrap4 的js函式庫以及設定全域符號$
- * */
 require('bootstrap');
 window.$ = window.jQuery = require('jquery');
