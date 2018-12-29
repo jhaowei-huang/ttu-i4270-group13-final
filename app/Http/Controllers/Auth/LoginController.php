2514a37205579a0eb4ca532f31960df50111e03c
jhaowei-huang <jhaowei.huang@gmail.com>
2018-12-30 01:34:09 +0800

    implement sign in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Rules\Captcha;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
@@ -23,34 +25,117 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
+     * 跳轉至首頁
      *
      * @var string
      */
     protected $redirectTo = '/';
 
     /**
-     * Create a new controller instance.
+     * 建構子
      *
      * @return void
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('signout');
+        // 中介層將會忽略登出，直接進行動作
+        // 其餘的動作將會受到guest的中介層保護
+        $this->middleware('guest', ['except' => 'signout']);
     }
 
     /**
-     * Log the user out of the application.
+     * 回傳自定義的帳號欄位，預設為'email'
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'username';
+    }
+
+    /**
+     * 自定義一個驗證器來驗證表單資料格式是否正確
+     *
+     * @param  array $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'g-recaptcha-response' => [new Captcha()],
+            'username' => ['required'],
+            'password' => ['required'],
+        ], [
+            'username.required' => trans('custom_validation.error_username_empty'),
+            'password.required' => trans('custom_validation.error_password_empty'),
+        ]);
+    }
+
+    /**
+     * 處理登入請求
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function signin(Request $request)
+    {
+        // 判斷輸入的是帳號還是email
+        $field = filter_var($request->get('username'), FILTER_VALIDATE_EMAIL) ? 'email' : $this->username();
+        // 將表單內容轉換成自訂的格式
+        $data = [
+            'g-recaptcha-response' => $request->get('g-recaptcha-response'),
+            'username' => $request->get('username'),
+            'password' => $request->get('password')
+        ];
+        // 驗證資料格式是否正確
+        $this->validator($data)->validate();
+        // 登入憑證，使用帳號或是email來登入
+        $credentials = [
+            $field => $request->get('username'),
+            'password' => $request->get('password')
+        ];
+        // 是否有勾選'在此裝置記住我'
+        $remember = $request->has('remember') ? true : false;
+        // 嘗試登入
+        if (Auth::attempt($credentials, $remember)) {
+            // 登入成功
+            if (Auth::user()->email_verified == 0) {
+                // email還尚未驗證，要求跳轉至verifyUserEmail頁面
+                return response()->json([
+                    'redirect' => '/verifyUserEmail',
+                    'errors' => []
+                ]);
+            } else {
+                // email已經驗證，要求跳轉至首頁
+                return response()->json([
+                    'redirect' => '/',
+                    'errors' => []
+                ]);
+            }
+        } else {
+            // 登入失敗，回報憑證錯誤
+            return response()->json([
+                'redirect' => '',
+                'errors' => [
+                    'username' => trans('custom_validation.error_username'),
+                    'password' => trans('custom_validation.error_password')
+                ]
+            ]);
+        }
+    }
+
+    /**
+     * 登出
      *
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function signout(Request $request)
     {
-        // $this->guard()->logout();
-        // $request->session()->invalidate();
-        // return $this->loggedOut($request) ?: redirect('/');
         Auth::logout();
+        $request->session()->invalidate();
         return redirect($this->redirectTo);
     }
 }
--- resources/views/pages/auth/signin.blade.php
+++ resources/views/pages/auth/signin.blade.php
@@ -9,14 +9,15 @@
         <div class="row justify-content-center">
             <div class="col-12 col-md-4 align-content-center">
                 <h3 class="text-center mb-3">登入會員</h3>
-                <form method="POST" action="{{ route('signin') }}">
+                <form id="form-signin" method="POST">
                     @csrf
                     <div class="form-group">
                         <div class="input-group input-group-lg">
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fa fa-user"></i></span>
                             </div>
-                            <input type="text" class="form-control" placeholder="帳號或是email">
+                            <input type="text" class="form-control" placeholder="帳號或是email"
+                                   id="username" name="username">
                         </div>
                     </div>
                     <div class="form-group">
@@ -24,23 +25,29 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fa fa-key"></i></span>
                             </div>
-                            <input type="password" class="form-control" placeholder="密碼">
+                            <input type="password" class="form-control" placeholder="密碼"
+                                   id="password" name="password">
                         </div>
                     </div>
-                    <div class="row justify-content-center my-3">
+                    <div class="row justify-content-center mt-3 mb-2">
                         <div class="g-recaptcha"
                              data-sitekey="{{env('GOOGLE_RECAPTCHA_KEY')}}"></div>
                     </div>
-                    <div class="validation-area my-2"></div>
+                    <div class="row mb-2 justify-content-center">
+                        <img id="loading" src="{{ asset('images/loading.gif') }}" alt="..."/>
+                        <div class="validation-area mt-1 mt-md-2"></div>
+                    </div>
                     <div class="form-inline mt-2">
                         <div class="input-group my-2 mt-md-0 flex-fill">
                             <label class="switch mr-2">
-                                <input type="checkbox" class="success" name="remember" id="remember" hidden>
+                                <input type="checkbox" class="success" id="remember" name="remember">
                                 <span class="slider round"></span>
                             </label>
                             <label id="remember-label" for="remember">在此裝置記住我</label>
                         </div>
-                        <button type="submit" class="btn btn-primary flex-fill flex-md-grow-0">登入</button>
+                        <button type="submit" class="btn btn-primary flex-fill flex-md-grow-0"
+                                id="btn-submit">登入
+                        </button>
                     </div>
                 </form>
                 @include('layouts.function', ['all' => false, 'signup' => true, 'forget' => true])
@@ -51,5 +58,6 @@
 
 @push('scripts')
     <!-- load Google recaotcha service -->
-    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <script src="{{ asset('js/signin.js') }}"></script>
 @endpush
