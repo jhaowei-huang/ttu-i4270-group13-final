bbdc10e2c6fd1cb7cefdc6e3216e92eb0a094d21
jhaowei-huang <jhaowei.huang@gmail.com>
2018-12-30 01:55:33 +0800

    implement forget password (not complete)

---
+++ app/PasswordReset.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordReset extends Model
+{
+    protected $primaryKey = 'password_reset_id';
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'email', 'email');
+    }
+}
--- resources/lang/en/custom_validation.php
+++ resources/lang/en/custom_validation.php
@@ -3,12 +3,15 @@ return [
     // connection error messages
     'error_db_connect_failed' => '無法連接到資料庫',
     // login form error messages
-    'error_username_password' => '錯誤的帳號或是密碼',
+    'error_username' => '請確認您的帳號或是email是否正確',
+    'error_username_empty' => '請輸入帳號或是email',
+    'error_password' => '請確認您的密碼是否正確',
+    'error_password_empty' => '請輸入密碼',
     // register account form error messages
     'error_username_validation' => '帳號只能是英數字，至少包含1個英文字以及1個數字，長度6~20個字元',
-    'error_username_exist' => '該帳號已經使用',
+    'error_username_exist' => '該帳號已經被其他人使用',
     'error_email_validation' => 'email格式錯誤',
-    'error_email_exist' => '該email已經使用',
+    'error_email_exist' => '該email已經被其他人使用',
     'error_password_confirm' => '兩次輸入的密碼不一樣',
     'error_password_same_as_username' => '密碼與帳號不能一樣',
     'error_password_validation' => '密碼至少包含1個英文字以及1個數字，長度6~20個字元',
@@ -26,4 +29,7 @@ return [
     'error_new_password_confirm' => '兩次輸入的新密碼不一樣',
     'error_new_password_validation' => '新密碼至少包含1個英文字以及1個數字，長度6~20個字元',
     'error_new_password_same' => '新密碼不能與帳號或舊密碼一樣',
+    // 重設密碼錯誤
+    'error_email' => '找不到使用該email的帳號',
+    'error_email_empty' => '請輸入email'
 ];
