758ac001e32214402b3211f0acf030a93ea80004
jhaowei-huang <jhaowei.huang@gmail.com>
2019-01-03 02:01:04 +0800

    disable footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,10 +2,11 @@
 <footer class="container">
     <p class="float-md-right float-lg-right"><a class="btn btn-outline-dark btn-sm" href="#">回到最上面</a></p>
     <p>&copy; 大同大學-軟體服務設計的創新實務-第13組
-        <a href="https://laravel.com/">Laravle框架</a>&middot;
-        <a href="https://getbootstrap.com/">Bootstrap</a>&middot;
-        <a href="https://www.heroku.com/platform">使用Heroku平台</a>&middot;
-        <a href="https://github.com/jhaowei-huang/ttu-course-i4270-group13">GitHub</a>
+        {{--<a href="https://laravel.com/">Laravle框架</a>&middot;--}}
+        {{--<a href="https://getbootstrap.com/">Bootstrap</a>&middot;--}}
+        {{--<a href="https://www.heroku.com/platform">使用Heroku平台</a>&middot;--}}
+        <a href="https://github.com/jhaowei-huang/ttu-i4270-group13-final">github</a>
+        <a href="https://gitlab.com/jhaoweihuang/ttu-i4270-group13-final">gitlab</a>
     </p>
     <p>此網站為大同大學軟體服務設計的創新實務課程之練習<br>原內容參考於<a href="http://www.tife.org.tw/tife2018/index.aspx">http://www.tife.org.tw/tife2018/index.aspx</a>
     </p>
